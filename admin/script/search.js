6c4d197b23784c2eecedcd42a194d7d1f3d3d431
Oniely <jabagatniel09@gmail.com>
2024-06-10 22:10:37 +0800

    admin global search

---
+++ admin/script/search.js
@@ -0,0 +1,39 @@
+$('#search-input').on('input', () => {
+    let searchValue = $('#search-input').val();
+
+    if (searchValue.length > 0) {
+        $('#search-result-container').removeClass('hidden');
+        $('#search-result-container').addClass('flex');
+    } else {
+        $('#search-result-container').removeClass('flex');
+        $('#search-result-container').addClass('hidden');
+    }
+
+    $.ajax({
+        url: 'includes/search.php',
+        type: 'GET',
+        data: { 
+            keyword: searchValue 
+        },
+        success: (data) => {
+            $('#search-result-container').html(data);
+        }
+    });
+});
+
+$('#search-input').on('focusout', () => {
+    setTimeout(() => {
+        $('#search-result-container').removeClass('flex');
+        $('#search-result-container').addClass('hidden');
+    }, 100);
+});
+
+
+$('#search-input').on('focusin', () => {
+    let searchValue = $('#search-input').val();
+
+    if (searchValue.length > 0) {
+        $('#search-result-container').removeClass('hidden');
+        $('#search-result-container').addClass('flex');
+    }
+});
